ab2bd57ebf1fb30fef5d756d2a187a26b038cfbb
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-05 23:23:44 +0300

    creates a DOM loaded event listener

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+// sets up a DOMContentLoaded event listener
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const paragraph = document.getElementById('text');// Selects the paragraph with '#text'
+    
+    paragraph.textContent = 'This is really cool!'; // Change the text content of the paragraph
+});
